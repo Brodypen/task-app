aace9f10a29cfbc9988a256bea5a203194eb4022
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-08 19:07:37 -0500

    Add react hooks

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import Overview from "./components/Overview";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks] = useState([]);
+  let [text] = useState("");
+  const handleChange = (e) => {
+    text = e.target.value;
+  };
+  const onSubmitTask = (e) => {
+    e.preventDefault();
+    tasks.push(text);
+    console.log(tasks);
+  };
   return (
     <div>
-
+      <form onSubmit={onSubmitTask}>
+        <label htmlFor="taskInput"></label>
+        <input type="text" id="taskInput" onChange={handleChange} />
+        <button type="submit">Add Task</button>
+        <Overview tasks={tasks} />
+      </form>
     </div>
   );
 }
